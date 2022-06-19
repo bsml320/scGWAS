d20ef319995ed63204e0f8fd41807d4dee14d551
bsml320 <peilin.jia@gmail.com>
2022-06-19 10:09:11 +0800

    Update 0_0.run_Magma.sh

--- analysis/0_0.run_Magma.sh
+++ analysis/0_0.run_Magma.sh
@@ -1,1 +1,7 @@
+#Example command line:
 
+# Step 1:
+magma.exe --annotate window=50,35 --snp-loc ..\path\to\snp_loc.txt --gene-loc ..\path\to\NCBI37.3\NCBI37.3.gene.loc --out clozuk_pgc2
+
+# Step 2:
+magma.exe --bfile ..\g1000_eur\g1000_eur --pval clozuk_pgc2.meta.sumstats.2.txt N=105318 --gene-annot clozuk_pgc2.genes.annot.txt --out clozuk_pgc2.Magma
